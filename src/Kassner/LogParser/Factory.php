69fb5496adb5f57e35301be848d7ab329f49de31
Don Bowlby <dbowlby@corexcel.com>
2019-11-26 18:07:16 -0500

    Factory: Refactor to check for existing parser

--- src/Kassner/LogParser/Factory.php
+++ src/Kassner/LogParser/Factory.php
@@ -4,15 +4,28 @@ declare(strict_types = 1);
 
 namespace Kassner\LogParser;
 
+/**
+ * The Factory class builds new LogParsers for us.
+ *
+ * @package Kassner\LogParser
+ * @author  Rafael Kassner <kassner@gmail.com>
+ * @author  Don Bowlby <dfbfloyd@gmail.com>
+ */
 class Factory
 {
+    private static $parser;
+
     /**
-     * Creates a LogParser instance.
+     * Return an instance of LogParser.
      *
      * @return \Kassner\LogParser\LogParser
      */
     public static function create(): LogParser
     {
-        return new LogParser();
+      if (!self::$parser) {
+        self::$parser = new LogParser();
+      }
+
+      return self::$parser;
     }
 }
